62fffa05893e4870dedf65f93cc406133c128c06
rickyk95 <rickyk95@hotmail.com>
2023-05-08 16:40:54 -0600

    new commit

--- src/controllers/scrape.js
+++ src/controllers/scrape.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer');
 async function searchThreads(url){
+    console.log('Calling searchThreads');
     const browser = await puppeteer.launch({headless:true});
     const page = await browser.newPage();
     await page.goto(url);
+    console.log('Just visited URL');
     const elements = await page.$$eval('a.SQnoC3ObvgnGjWt90zD9Z._2INHSNB8V5eaWp4P0rY_mE',(arr)=>{ 
         let links = [];
         arr.forEach((element)=>{
@@ -13,8 +15,12 @@ async function searchThreads(url){
     return elements;
 }    
 module.exports=async(req,res)=>{
-    let threads = await searchThreads(req.body.url);
-    console.log(req.body.url);
-    res.status(200).send(threads);
+    try{
+    // let threads = await searchThreads(req.body.url);
+    // console.log(req.body.url);
+    res.status(200).send('hello there buddy');
+    }catch(e){
+        console.log(e,'error');
+    }
 }
 
