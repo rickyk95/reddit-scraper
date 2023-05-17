b25a4e1a8d661ad70e13e23fc41e37a76f9d5e6b
rickyk95 <rickyk95@hotmail.com>
2023-05-17 13:27:31 -0600

    new commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const puppeteer = require('puppeteer');
 const exphbs = require('express-handlebars');
 const router = require('./routes/routes');
 const port = process.env.PORT || 3000;
@@ -10,3 +11,15 @@ app.use(router);
 app.listen(port,()=>{
         console.log(`Listening on port ${port}`);
 });
+
+// async function launchBrowser(){
+//        let browser =  await puppeteer.launch({headless:false});
+//        let page = await browser.newPage();
+//        await page.goto('https://www.google.com');
+//        await page.type('#APjFqb','Hondu Autos');
+//        await page.keyboard.press('Enter');
+//        await page.waitForNavigation();
+//        await page.click('a[jsname=qOiK6e]');
+// }
+
+// launchBrowser();
