3c639164f48fa37867007b8c1fa00c5cc93aef02
Richard Kafie <rickyk95@hotmail.com>
2023-05-24 10:51:34 -0600

    new commit

--- src/index.js
+++ src/index.js
@@ -11,23 +11,6 @@ app.use(router);
 app.listen(port,()=>{
         console.log(`Listening on port ${port}`);
 });
-
 const Sentiment = require('sentiment');
-
 var sentiment = new Sentiment();
-
-let score = sentiment.analyze('David Pkaman is awesome and very smart ');
-
-console.log(score);
-
-// async function launchBrowser(){
-//        let browser =  await puppeteer.launch({headless:false});
-//        let page = await browser.newPage();
-//        await page.goto('https://www.google.com');
-//        await page.type('#APjFqb','Hondu Autos');
-//        await page.keyboard.press('Enter');
-//        await page.waitForNavigation();
-//        await page.click('a[jsname=qOiK6e]');
-// }
-
-// launchBrowser();
+let score = sentiment.analyze('David Pkaman is awesome and very smart');
